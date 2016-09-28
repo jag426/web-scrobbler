620ed0dd5f9f21d466fde2e160ee86c70dffa78f
J. Aaron Gustafson <jag426@cornell.edu>
2016-09-28 13:04:08 -0700

    UNTESTED Fix google music
    Get metadata from queue

--- connectors/v2/googlemusic.js
+++ connectors/v2/googlemusic.js
@@ -5,25 +5,21 @@
 Connector.playerSelector = '#player';
 
 Connector.getTrackArt = function() {
-	return $('#playerBarArt').attr('src') ||
-		$('#playingAlbumArt').attr('src') || null;
+	return $('.song-row.currently-playing .column-content img').attr('src') || null;
 };
 
-Connector.artistSelector = '#player-artist';
+Connector.artistSelector = '.song-row.currently-playing .column-content .song-artist';
 
-Connector.getTrack = function() {
-	return $('#currently-playing-title').text() ||
-		$('#player-song-title').text() || null;
-};
+Connector.trackSelector = '.song-row.currently-playing .column-content .song-title';
 
-Connector.albumSelector = '.player-album';
+Connector.albumSelector = '.song-row.currently-playing .column-content .song-album';
 
 Connector.currentTimeSelector = '#time_container_current';
 
 Connector.durationSelector = '#time_container_duration';
 
 Connector.isPlaying = function() {
-	return $('#player *[data-id="play-pause"]').hasClass('playing');
+	return $('#player *[data-id="play-pause"]').attr('playback-state') == 'playing';
 };
 
 Connector.isStateChangeAllowed = function() {
